7cface3165cdf8275bdd7dd4a148a2d859c1db1a
YTianHua <2273704863@qq.com>
2019-05-30 20:09:35 +0800

    Initial commit

---
+++ app/src/main/java/com/yth520web/page/MainActivity.java
@@ -0,0 +1,27 @@
+package com.yth520web.page;
+
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ViewPager viewPager= (ViewPager)findViewById(R.id.viewpager);
+        //创建Adapter对象，绑定ViewPager和pageAdapter
+        ViewPager viewpager = (ViewPager)findViewById(R.id.viewpager);
+        MyPagerAdapter pagerAdapter = new MyPagerAdapter(getSupportFragmentManager());
+        //联系viewpager和pageAdapter
+        viewPager.setAdapter(pagerAdapter);
+
+        TabLayout tabLayout = (TabLayout)findViewById(R.id.sliding_tabs);
+        //绑定tabLayout和当前的viewPager,滑动后一起联动
+        tabLayout.setupWithViewPager(viewPager);
+
+    }
+}
